a357abfac8ebb9db810112195fcd887724326814
soyoung lee <decfugu@gmail.com>
2016-05-09 02:21:31 +0900

    loop() 정리 checkHic() 생성 - 체감온도 리

--- _122268.ino
+++ _122268.ino
@@ -1,4 +1,4 @@
-#include <DHT.h> //DHT 라이브러리
+#include <DHT.h> // DHT 라이브러리
 
 #include <Adafruit_NeoPixel.h> // 네오픽셀 라이브러리
 #ifdef __AVR__
@@ -8,33 +8,29 @@
 #include <Adafruit_CC3000.h> //와이파이 라이브러리
 #include <Adafruit_CC3000_Server.h>
 #include <ccspi.h>
+
 #include <string.h>
 #include "utility/debug.h"
 
 #define DHTPIN 2            //DHT 11 핀
-#define DHTTYPE DHT11 
+#define DHTTYPE DHT11
 
 #define LEDPIN 6            //LED 핀
 #define NUMPIXELS 1
 
-// These are the interrupt and control pins
-#define ADAFRUIT_CC3000_IRQ   3  // MUST be an interrupt pin!
-// These can be any two pins
+#define ADAFRUIT_CC3000_IRQ   3  //WIFI 핀
 #define ADAFRUIT_CC3000_VBAT  5
 #define ADAFRUIT_CC3000_CS    10
-// Use hardware SPI for the remaining pins
-// On an UNO, SCK = 13, MISO = 12, and MOSI = 11
 Adafruit_CC3000 cc3000 = Adafruit_CC3000(ADAFRUIT_CC3000_CS, ADAFRUIT_CC3000_IRQ, ADAFRUIT_CC3000_VBAT,
-                                         SPI_CLOCK_DIVIDER); // you can change this clock speed
+                         SPI_CLOCK_DIVIDER); // you can change this clock speed
 
-#define WLAN_SSID       "makye"          // cannot be longer than 32 characters!
-#define WLAN_PASS       "dkaghdkagh"
-// Security can be WLAN_SEC_UNSEC, WLAN_SEC_WEP, WLAN_SEC_WPA or WLAN_SEC_WPA2
+#define WLAN_SSID       "N704M"          // cannot be longer than 32 characters!
+#define WLAN_PASS       "skek2sha"
 #define WLAN_SECURITY   WLAN_SEC_WPA2
 
 #define IDLE_TIMEOUT_MS  3000      // Amount of time to wait (in milliseconds) with no data 
-                                   // received before closing the connection.  If you know the server
-                                   // you're accessing is quick to respond, you can reduce this value.
+// received before closing the connection.  If you know the server
+// you're accessing is quick to respond, you can reduce this value.
 
 // What page to grab!
 #define WEBSITE      "www.kma.go.kr"
@@ -43,92 +39,100 @@ Adafruit_CC3000 cc3000 = Adafruit_CC3000(ADAFRUIT_CC3000_CS, ADAFRUIT_CC3000_IRQ
 int dim;
 DHT dht(DHTPIN, DHTTYPE);
 
-int delayval= 500;
+int delayval = 500; // Neopixel 딜레이
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, LEDPIN, NEO_GRB + NEO_KHZ800);
 
 uint32_t ip;
 
-
-
-
-
-void coldLed(){
-    for(int i=0; i<NUMPIXELS; i++){
-    pixels.setPixelColor(i, pixels.Color(16,35,64) );
+void coldLed() {
+  for (int i = 0; i < NUMPIXELS; i++) {
+    pixels.setPixelColor(i, pixels.Color(16, 35, 64) );
     pixels.show();
-    }
+  }
 }
 
-void coolLed(){  
-    for(int i=0; i<NUMPIXELS; i++){
-    pixels.setPixelColor(i, pixels.Color(62,76,127) );
+void coolLed() {
+  for (int i = 0; i < NUMPIXELS; i++) {
+    pixels.setPixelColor(i, pixels.Color(62, 76, 127) );
     pixels.show();
-    }
+  }
 }
 
-void fineLed(){
-    for(int i=0; i<NUMPIXELS; i++){
-    pixels.setPixelColor(i, pixels.Color(201,201,201) );
+void fineLed() {
+  for (int i = 0; i < NUMPIXELS; i++) {
+    pixels.setPixelColor(i, pixels.Color(201, 201, 201) );
     pixels.show();
-    }
+  }
 }
 
-void warmLed(){
-    for(int i=0; i<NUMPIXELS; i++){
-    pixels.setPixelColor(i, pixels.Color(255,166,106) );
+void warmLed() {
+  for (int i = 0; i < NUMPIXELS; i++) {
+    pixels.setPixelColor(i, pixels.Color(255, 166, 106) );
     pixels.show();
-    }
+  }
 }
 
-void hotLed(){
-    for(int i=0; i<NUMPIXELS; i++){
-    pixels.setPixelColor(i, pixels.Color(235,121,46) );
+void hotLed() {
+  for (int i = 0; i < NUMPIXELS; i++) {
+    pixels.setPixelColor(i, pixels.Color(235, 121, 46) );
     pixels.show();
-    }
+  }
 }
 
+float checkHic()
+{
+  // Wait a few seconds between measurements.
+  delay(2000);
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
 
-
-
-
+  // Check if any reads failed and exit early (to try again).
+  if ( isnan(h) || isnan(t) ) {
+    Serial.println("Failed to read from DHT sensor!");
+    return 0;
+  }
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+  return hic;
+}
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600); //디버그 시리얼
 
-//-------------------------------------------------------
   dht.begin();
   Serial.println("DHT11 start");
-//-------------------------------------------------------
+
   pixels.begin();
   Serial.println("Neopixel start");
-//-------------------------------------------------------
+
   if (!cc3000.begin())
   {
     Serial.println(F("Couldn't begin()! Check your wiring?"));
-    while(1);
+    while (1);
   }
-  
+
   // Optional SSID scan
   // listSSIDResults();
-  
+
   Serial.print(F("\nAttempting to connect to ")); Serial.println(WLAN_SSID);
   if (!cc3000.connectToAP(WLAN_SSID, WLAN_PASS, WLAN_SECURITY)) {
     Serial.println(F("Failed!"));
-    while(1);
+    while (1);
   }
   Serial.println(F("Connected!"));
-  
+
   /* Wait for DHCP to complete */
   Serial.println(F("Request DHCP"));
   while (!cc3000.checkDHCP())
   {
     delay(100); // ToDo: Insert a DHCP timeout!
-  }  
+  }
 
-  /* Display the IP address DNS, Gateway, etc. 
-  while (! displayConnectionDetails()) {
+  /* Display the IP address DNS, Gateway, etc.
+    while (! displayConnectionDetails()) {
     delay(1000);
-  }*/
+    }*/
 
   ip = 0;
   // Try looking up the website's IP address
@@ -141,13 +145,13 @@ void setup() {
   }
 
   cc3000.printIPdotsRev(ip);
-  
+
   // Optional: Do a ping test on the website
   /*
-  Serial.print(F("\n\rPinging ")); cc3000.printIPdotsRev(ip); Serial.print("...");  
-  replies = cc3000.ping(ip, 5);
-  Serial.print(replies); Serial.println(F(" replies"));
-  */  
+    Serial.print(F("\n\rPinging ")); cc3000.printIPdotsRev(ip); Serial.print("...");
+    replies = cc3000.ping(ip, 5);
+    Serial.print(replies); Serial.println(F(" replies"));
+  */
 
   /* Try connecting to the website.
      Note: HTTP/1.1 protocol is used to keep the server from closing the connection before all data is read.
@@ -161,13 +165,13 @@ void setup() {
     www.fastrprint(F("\r\n"));
     www.println();
   } else {
-    Serial.println(F("Connection failed"));    
+    Serial.println(F("Connection failed"));
     return;
   }
 
   Serial.println(F("-------------------------------------"));
-  
-  /* Read data until either the connection is closed, or the idle timeout is reached. */ 
+
+  /* Read data until either the connection is closed, or the idle timeout is reached. */
   unsigned long lastRead = millis();
   while (www.connected() && (millis() - lastRead < IDLE_TIMEOUT_MS)) {
     while (www.available()) {
@@ -178,7 +182,7 @@ void setup() {
   }
   www.close();
   Serial.println(F("-------------------------------------"));
-  
+
   /* You need to make sure to clean up after yourself or the CC3000 can freak out */
   /* the next time your try to connect ... */
   Serial.println(F("\n\nDisconnecting"));
@@ -187,46 +191,7 @@ void setup() {
 }
 
 void loop() {
-  // Wait a few seconds between measurements.
-  delay(2000);
 
-  float h = dht.readHumidity();
-  // Read temperature as Celsius (the default)
-  float t = dht.readTemperature();
-  // Read temperature as Fahrenheit (isFahrenheit = true)
-  float f = dht.readTemperature(true);
+  Serial.println(checkHic());
+}
 
-  // Check if any reads failed and exit early (to try again).
-  if (isnan(h) || isnan(t) || isnan(f)) {
-    Serial.println("Failed to read from DHT sensor!");
-    return;
-  }
-
-  // Compute heat index in Fahrenheit (the default)
-  float hif = dht.computeHeatIndex(f, h);
-  // Compute heat index in Celsius (isFahreheit = false)
-  float hic = dht.computeHeatIndex(t, h, false);
-
-  Serial.print("Humidity: ");
-  Serial.print(h);
-  Serial.print(" %\t");
-  Serial.print("Temperature: ");
-  Serial.print(t);
-  Serial.print(" *C ");
-  Serial.print(f);
-  Serial.print(" *F\t");
-  Serial.print("Heat index: "); //체감온도
-  Serial.print(hic);
-  Serial.print(" *C ");
-  Serial.print(hif);
-  Serial.println(" *F");
-  
-  
-  for(int i=0; i<NUMPIXELS; i++){
-    pixels.setPixelColor(i, pixels.Color(0,121,46) );
-    pixels.show();
-  
-    }
- }
-
- 
